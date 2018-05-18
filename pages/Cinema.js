9604eb24a5f41458be5803c2c09430ad8737ed86
Yumingkun <1610252385@vip.henu.edu.cn>
2018-05-18 20:15:51 +0800

    readme

--- pages/Cinema.js
+++ pages/Cinema.js
@@ -24,14 +24,38 @@ export default class Cinemas extends Component {
     render() {
         const {state,goBack}=this.props.navigation;
         return (
-            <View  >
-                <Text>影院页</Text>
+            <View  style={styles.parent}>
+                <Text style={styles.view1}>我的影院1</Text>
+                <Text style={styles.view2}>我的影院2</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    parent:{
+        flex:1,
+    },
+    view1:{
+        backgroundColor:'#676677',
+        height:100,
+        margin:10,
+        borderRadius:10,
+
+        elevation:5,//漂浮的效果
+        shadowColor:'red',
+
+    },
+    view2:{
+        backgroundColor:'#9fbb74',
+        height:100,
+        marginRight:10,
+        marginLeft:10,
+        borderRadius:10,
+
+        elevation:5,
+
+    }
 
 
 });
--- pages/MyMovies.js
+++ pages/MyMovies.js
@@ -24,8 +24,15 @@ export default class Cinemas extends Component {
     render() {
         const {state,goBack}=this.props.navigation;
         return (
-            <View  >
-                <Text>我的电影页</Text>
+            <View  style={styles.parent}>
+
+                    <Text style={[styles.childfirst,styles.margin]}> View1 </Text>
+                    <Text style={[styles.childsecond,styles.margin]}> View2 </Text>
+                    <Text style={[styles.childthird,styles.margin]}> View3 </Text>
+                    <Text style={styles.childthird}> View3 </Text>
+                    <Text style={styles.childthird}> View3 </Text>
+                    <Text style={styles.childthird}> View3 </Text>
+
             </View>
         );
     }
@@ -33,5 +40,39 @@ export default class Cinemas extends Component {
 
 const styles = StyleSheet.create({
 
+    parent:{
+        flex:1,
+        flexDirection:'row',
+        opacity:0.4,
 
+        flexWrap:'wrap',//换行
+        justifyContent:'space-around',
+    },
+    childfirst:{
+        backgroundColor:'#676677',
+        width:100,
+        height:100,
+        fontSize:13,
+
+    },
+    childsecond:{
+        backgroundColor:'#9fbb74',
+        width:100,
+        height:100,
+        fontSize:13,
+
+    },
+    childthird:{
+        backgroundColor:'#bb7478',
+        width:100,
+        height:100,
+        fontSize:13,
+
+    },
+    margin:{
+
+        marginBottom:10,
+    }
 });
+
+
